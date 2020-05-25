e9ca49855600ce90c1bce57448616f5a96b823f9
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-05-25 22:46:00 +0530

    Update MatchAll.js

--- MatchAll.js
+++ MatchAll.js
@@ -10,3 +10,6 @@ shortHand.test(varNames); // Returns true
 
 
 
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g; 
+let result = quoteSample.match(alphabetRegexV2).length;
