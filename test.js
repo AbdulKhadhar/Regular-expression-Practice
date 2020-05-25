4ca75bed9d7a317d27b7aa519c67af0b0ee0deaa
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-05-25 12:28:50 +0530

    Using the Test Method

---
+++ test.js
@@ -0,0 +1,10 @@
+let testStr = "freeCodeCamp";
+let testRegex = /Code/;
+testRegex.test(testStr);
+// Returns true
+
+
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString);
