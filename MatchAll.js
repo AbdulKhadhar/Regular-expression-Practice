2511dad97aef1ab6d703325cb90177d279d99b54
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-05-25 22:43:16 +0530

    Match All Letters and Numbers

---
+++ MatchAll.js
@@ -0,0 +1,12 @@
+let longHand = /[A-Za-z0-9_]+/;
+let shortHand = /\w+/;
+let numbers = "42";
+let varNames = "important_var";
+longHand.test(numbers); // Returns true
+shortHand.test(numbers); // Returns true
+longHand.test(varNames); // Returns true
+shortHand.test(varNames); // Returns true
+
+
+
+
